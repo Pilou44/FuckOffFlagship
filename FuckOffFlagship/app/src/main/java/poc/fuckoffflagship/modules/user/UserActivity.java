3db18b67c89955358b2a6325aae13a2c3a872574
Guillaume BEGUIN <gbeguin@viadeoteam.com>
2017-07-12 10:42:06 +0200

    Make fragment view resistant to rotation

--- FuckOffFlagship/app/src/main/java/poc/fuckoffflagship/modules/user/UserActivity.java
+++ FuckOffFlagship/app/src/main/java/poc/fuckoffflagship/modules/user/UserActivity.java
@@ -20,16 +20,9 @@ public class UserActivity extends BaseActivity implements UserContract.UserView 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        /*FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });*/
-
-        parseIntent(getIntent());
+        if (savedInstanceState == null) {
+            parseIntent(getIntent());
+        }
     }
 
     @Override
